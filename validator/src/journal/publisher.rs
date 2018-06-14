7091774ba5a8a3f62e500ccebef6bfcc597fac02
Boyd Johnson <bjohnson@bitwise.io>
2018-06-14 15:49:17 -0500

    Add cancel_block to python and rust ffi

--- validator/src/journal/publisher.rs
+++ validator/src/journal/publisher.rs
@@ -50,6 +50,11 @@ pub enum InitializeBlockError {
 }
 
 #[derive(Debug)]
+pub enum CancelBlockError {
+    BlockNotInProgress,
+}
+
+#[derive(Debug)]
 pub enum FinalizeBlockError {
     BlockNotInitialized,
     BlockEmpty,
@@ -567,6 +572,16 @@ impl BlockPublisher {
                 warn!("PublisherThread exiting");
             })
             .unwrap();
+    }
+
+    pub fn cancel_block(&self) -> Result<(), CancelBlockError> {
+        let mut state = self.publisher.state.write().expect("RwLock was poisoned");
+        if state.candidate_block.is_some() {
+            self.publisher.cancel_block(&mut state);
+            Ok(())
+        } else {
+            Err(CancelBlockError::BlockNotInProgress)
+        }
     }
 
     pub fn stop(&self) {
